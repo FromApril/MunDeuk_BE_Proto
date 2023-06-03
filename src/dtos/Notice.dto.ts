14b215eace894b4d4dbd12a781e23c4d1d60a461
Lee seung chan <dltmdcks702@gmail.com>
2023-06-04 08:35:49 +0900

    feat : 알림 생성 함수 추가

---
+++ src/dtos/Notice.dto.ts
@@ -0,0 +1,44 @@
+import {Notification, NotificationType} from "@prisma/client"
+import { Exclude, Expose } from "class-transformer";
+
+@Exclude()
+class NoticeDTO implements Notification {
+  @Expose()
+  createdAt: Date;
+
+  @Expose()
+  updatedAt: Date;
+
+  @Expose()
+  title: string;
+
+  @Expose()
+  detail: string;
+
+  @Expose()
+  notificationType: NotificationType;
+
+  @Expose()
+  notificationDetail: string;
+
+  id: bigint;
+  noticeableId: bigint;
+  notifiedMemberId: bigint;
+
+  static createDTO({title, detail, notificationType, notificationDetail}: {
+    title: string ;
+    detail: string;
+    notificationType: NotificationType;
+    notificationDetail:string
+  }) {
+    const notice = new NoticeDTO();
+    notice.title = title;
+    notice.detail = detail;
+    notice.notificationType = notificationType;
+    notice.notificationDetail = notificationDetail;
+
+    return notice;
+  }
+}
+
+export default NoticeDTO;
---
+++ src/repositories/Notice.repository.ts
@@ -0,0 +1,26 @@
+import { Injectable } from "@nestjs/common";
+import NoticeDTO from "src/dtos/Notice.dto";
+import { PrismaService } from "src/prisma.service";
+
+@Injectable()
+class NoticeRepository {
+  constructor(private prismaService: PrismaService) {}
+
+  async publish({ listenerIdList, noticeableId, title, detail, notificationType, notificationDetail} : Pick<NoticeDTO, 'title'|'detail'|'notificationType'|'notificationDetail'> & {
+    listenerIdList: bigint[];
+    noticeableId: bigint;
+  }): Promise<void> {
+    await this.prismaService.notification.createMany({
+      data: listenerIdList.map((notifiedMemberId) => ({
+        title,
+        detail,
+        noticeableId,
+        notificationType,
+        notifiedMemberId,
+        notificationDetail,
+      }))
+    });
+  }
+}
+
+export default NoticeRepository;
