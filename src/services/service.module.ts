b426a11a22421c51c4e1d32977e1300a18a295ee
Lee seung chan <dltmdcks702@gmail.com>
2023-06-06 11:30:07 +0900

    feat: add import

--- src/services/service.module.ts
+++ src/services/service.module.ts
@@ -1,9 +1,12 @@
 import { Module } from "@nestjs/common";
 import NoteViewerService from "./NoteViewer.service";
 import NoteEditorService from "./NoteEditor.service";
+import MemberActionOnNoteService from "./MemberActionOnNote.service";
+import { RepositoryModule } from "src/repositories/repository.module";
 
 @Module({
-  providers: [NoteViewerService, NoteEditorService],
-  exports: [NoteViewerService, NoteEditorService],
+  imports: [RepositoryModule],
+  providers: [NoteViewerService, NoteEditorService, MemberActionOnNoteService],
+  exports: [NoteViewerService, NoteEditorService, MemberActionOnNoteService],
 })
 export class ServiceModule {}
