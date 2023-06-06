bee546fab51a2f4e6302eed16bf3650ae3f7ed11
Lee seung chan <dltmdcks702@gmail.com>
2023-06-06 14:17:43 +0900

    feat: add note mark all to be read

--- src/services/NoteEditor.service.ts
+++ src/services/NoteEditor.service.ts
@@ -33,6 +33,39 @@ class NoteEditorService {
     });
   }
 
+  async markAllRead({
+    viewerId,
+    noteIdList,
+  }: {
+    noteIdList: bigint[];
+    viewerId: bigint;
+  }) {
+    await this.prismaService.$transaction([
+      this.prismaService.locker.update({
+        where: {
+          ownerId: viewerId,
+        },
+        data: {
+          viewedNoteIdList: {
+            push: noteIdList,
+          },
+        },
+      }),
+      ...noteIdList.map((noteId) =>
+        this.prismaService.note.update({
+          where: {
+            id: noteId,
+          },
+          data: {
+            viewCount: {
+              increment: 1,
+            },
+          },
+        }),
+      ),
+    ]);
+  }
+
   async delete({ memberId, noteId }: DeleteNoteDTO) {
     await this.prismaService.note.findFirstOrThrow({
       where: { id: noteId, writerId: memberId },
